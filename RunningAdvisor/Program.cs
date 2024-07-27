2ccdee21b13f5d565898bfe1e1d354aebdb0aac3
RoTaXx <subofrotax@gmail.com>
2024-07-27 22:03:48 +0300

    Identity Scaffolding

--- RunningAdvisor/Program.cs
+++ RunningAdvisor/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RunningAdvisor.Data;
 using RunningAdvisor.Services;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("RunningAdvisor")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<RunningService>();
 builder.Services.AddScoped<BeginnerRunningService>();
